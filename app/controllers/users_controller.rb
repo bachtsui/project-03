e01bc705acb4ca5d2b62f2d947dc8700a0dc608b
bachtsui <dbtsui2@gmail.com>
2016-02-19 15:10:35 -0800

    Defined a create function for Users, also segmented out users params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,26 @@ class UsersController < ApplicationController
 		render :new
 	end	
 
+	def create
+		@user = User.new(user_params)
+
+		redirect_to user_path(@user)
+
+	# Use this Code when we create sessions
+
+	# if @user.save
+  #     login(@user) 
+  #     redirect_to @user
+  #   else
+  #     flash[:error] = @user.errors.full_messages.join(", ")
+  #     redirect_to new_user_path
+  #   end
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+	end
+
 end
