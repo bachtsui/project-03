0028ba50b7155bc64e5dd3e3c238092895ec7eb5
bachtsui <dbtsui2@gmail.com>
2016-02-19 13:48:57 -0800

    Added index and new routes for users, created User controller as well

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
 
   # Routes for Games
   # Shouldn't need Create, Update, or Destroy since we'll seed the data from Giant Bomb's API
-  get "/games", to: "games#index", as:"games"
-  get "/games/:id", to: "games#show", as:"game"
+  get "/games", to: "games#index", as: "games"
+  get "/games/:id", to: "games#show", as: "game"
 
+  #Routes for Users
+  get "/users", to: "user#index", as: "users"
+  get "/signup", to: "users#new", as: "new_user"
+  
 end
