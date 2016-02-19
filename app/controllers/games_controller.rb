fd5122591d6b846c036145a195172dacdee2de78
bachtsui <dbtsui2@gmail.com>
2016-02-19 11:30:52 -0800

    Create route, method, and view for games show

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,4 +6,9 @@ class GamesController < ApplicationController
 		#Can take render out later, if naming convention is right
 	end
 
+	def show
+		game_id = params[:id]
+		@game = Game.find_by_id(game_id)
+		render :show
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   root to: "game#index"
 
   # Routes for Games
+  # Shouldn't need Create, Update, or Destroy since we'll seed the data from Giant Bomb's API
   get "/games", to: "games#index", as:"games"
+  get "/games/:id", to: "games#show", as:"game"
+
 end
