27200b561626dbf4573e340056fbe9680181b7e6
bachtsui <dbtsui2@gmail.com>
2016-02-19 10:21:13 -0800

    Created some seed data for games

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#Destroy Functions to Prevent Duplicate Data
+
+Game.destroy_all
+
+Game.create({
+	name: "Super Mario RPG",
+	company: "Square",
+	publisher: "Nintendo",
+	image: "https://upload.wikimedia.org/wikipedia/en/8/89/SuperMarioRPGSNESCoverArtUS.jpg",
+	release_date: "May 13, 1996",
+})
+
+Game.create({
+	name: "Jet Set Radio",
+	company: "Smilebit",
+	publisher: "Sega",
+	image: "http://static.tvtropes.org/pmwiki/pub/images/JetGrindRadio_cover.jpg",
+	release_date: "October 30, 2000",
+})
+
+Game.create({
+	name: "Devil May Cry 3",
+	company: "Capcom",
+	publisher: "Capcom",
+	image: "https://upload.wikimedia.org/wikipedia/en/7/76/Devil_May_Cry_3_boxshot.jpg",
+	release_date: "February 17, 2005",
+})
