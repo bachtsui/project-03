4cacc414293eea1ca0aca7ac8dab868664b0987f
bachtsui <dbtsui2@gmail.com>
2016-02-19 15:02:53 -0800

    Reused code to create a better form for new user

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   #Routes for Users
   get "/users", to: "user#index", as: "users"
   get "/signup", to: "users#new", as: "new_user"
-  
+  post "/users", to: "users#create"
 end
