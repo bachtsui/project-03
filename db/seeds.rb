546ad0b31429cab07802cf9c5d51c7ec6a65cb44
bachtsui <dbtsui2@gmail.com>
2016-02-25 09:37:17 -0800

    Updated seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -42,56 +42,56 @@ Game.create({
 	name: "Street Fighter II",
 	company: "Capcom",
 	publisher: "Capcom",
-	image: "",
-	release_date: ""
+	image: "http://static.gamesradar.com/images/mb/GamesRadar/us/Features/2009/02/Street%20Fighter%20boxes/sf2--article_image.jpg",
+	release_date: "Februray 6,1991"
 })
 
 Game.create({
 	name: "Super Smash Bros. Melee",
-	company: "HAL",
+	company: "HAL Laboratory",
 	publisher: "Nintendo",
-	image: "",
-	release_date: ""
+	image: "https://vgboxart.com/boxes/GCN/1323-super-smash-bros-melee-2.jpg",
+	release_date: "December 3, 2001"
 })
 
 Game.create({
-	name: "Sonic the Hedgehog II",
-	company: "Sega",
+	name: "Sonic the Hedgehog 2",
+	company: "Sonic Team",
 	publisher: "Sega",
-	image: "",
-	release_date: ""
+	image: "http://gamefiles.alphacoders.com/boxart/original/front/thumb-142-2.jpg",
+	release_date: "November 24th, 1992"
 })
 
 Game.create({
 	name: "Pokemon Blue",
 	company: "Gamefreak",
 	publisher: "Nintendo",
-	image: "",
-	release_date: ""
+	image: "http://cdn.bulbagarden.net/upload/thumb/5/5a/Blue_EN_boxart.png/250px-Blue_EN_boxart.png",
+	release_date: "September 28, 1998"
 })
 
 Game.create({
 	name: "Undertale",
 	company: "Toby Fox",
-	publisher: "N/A",
-	image: "",
-	release_date: ""
+	publisher: "Toby Fox",
+	image: "http://media.breitbart.com/media/2015/11/undertale-kickstarter-640x480.png",
+	release_date: "September 15,2015"
 })
 
 Game.create({
 	name: "Starcraft",
 	company: "Blizzard",
 	publisher: "Blizzard",
-	image: "",
-	release_date: ""
+	image: "http://www.mobygames.com/images/covers/l/143850-starcraft-windows-front-cover.jpg",
+	release_date: "March 31,1998"
 })
 
 Game.create({
-	name: "The Legend of Zelda: Wind Waker",
+	name: "The Legend of Zelda: The Wind Waker",
 	company: "Nintendo",
 	publisher: "Nintendo",
-	image: "",
-	release_date: ""
+	image: "http://vignette2.wikia.nocookie.net/zelda/images/6/6d/The_Legend_of_Zelda_-_The_Wind_Waker_HD_(North_America).png/revision/latest?cb=20130808221458",
+	release_date: "March 24,2003"
 })
 
 
